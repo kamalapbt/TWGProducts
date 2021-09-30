1b24aaa21e27868da9df97941435786ab6191139
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2021-09-30 20:26:53 +1300

    Search screen design integration

--- TWGProducts/TWGProducts/Module/Search/ProductsViewController.swift
+++ TWGProducts/TWGProducts/Module/Search/ProductsViewController.swift
@@ -8,18 +8,41 @@
 import UIKit
 
 class ProductsViewController: UIViewController, Viewable {
+    @IBOutlet weak var tableView: UITableView!
     var presenter: ProductsPresenter?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
-    func didRecieveData(with data: SearchResult) {
+    
+    @IBAction func onTapBarcodeScanner(_ sender: Any) {
         
     }
+    
+    func didRecieveData(with data: SearchResult) {
+
+    }
+    
     func didRecieveError(with error: Error) {
         
     }
-
+    
+    
 }
 
+extension ProductsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "searchResultCell", for: indexPath)
+        cell.textLabel?.text = "Dummy text \(indexPath.row)"
+        return cell
+    }
+}
