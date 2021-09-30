14d4a403f229b4e5e7805dfabb798fe83df69f48
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2021-09-30 21:27:25 +1300

    enable backword compatibility for the project to iOS 12.1

--- TWGProducts/TWGProducts/SceneDelegate.swift
+++ TWGProducts/TWGProducts/SceneDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -22,7 +23,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let nav = UINavigationController(rootViewController: productsRouter.entry!)
         let initialVC = nav
-//        let initialVC = productsRouter.entry
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = initialVC
         self.window = window
